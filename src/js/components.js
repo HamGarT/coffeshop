2af7d4740e2796f38c14324927203d9a659c699d
Hamer Garcia <hamgar28@gmail.com>
2024-09-03 01:41:57 -0500

    shop almost done

---
+++ src/js/components.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to fetch and load HTML into the container
+    const loadHTML = (url, containerId) => {
+      fetch(url)
+        .then(response => response.text())
+        .then(data => {
+          const container = document.getElementById(containerId);
+          if (container) {
+            container.innerHTML = data;
+          } else {
+            console.error(`Element with ID ${containerId} not found.`);
+          }
+        })
+        .catch(error => console.error(`Error loading ${url}:`, error));
+    };
+
+    // Load navbar and footer
+    loadHTML('../../components/navbar.html', 'navbar-container');
+    loadHTML('../../components/footer.html', 'footer-container');
+
+    // Handle scroll event for sticky navbar
+    const handleScroll = () => {
+      const navbar = document.getElementById('navbar-container');
+      if (navbar) {
+        if (window.scrollY >= 100) {
+          navbar.classList.add('sticky');
+        } else {
+          navbar.classList.remove('sticky');
+        }
+      }
+    };
+
+    // Throttle scroll event handler
+    let isThrottled = false;
+    window.addEventListener('scroll', () => {
+      if (!isThrottled) {
+        handleScroll();
+        isThrottled = true;
+        setTimeout(() => {
+          isThrottled = false;
+        }, 100); // Adjust throttle delay as needed
+      }
+    });
+  });
---
+++ src/js/shop.js
@@ -0,0 +1,41 @@
+async function fetchData() {
+    try {
+        const response = await fetch("https://fake-coffee-api.vercel.app/api?limit=9");
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+function generateHTML(data) {
+    const container = document.getElementById('coffee-list'); 
+    container.innerHTML = '';
+
+    data.forEach(item => {
+        
+        const coffeeItem = document.createElement('div');
+        coffeeItem.classList.add('coffee-item');
+
+        const imgCoffe = document.createElement('img');
+        imgCoffe.src = item.image_url;
+        coffeeItem.appendChild(imgCoffe);
+       
+        const title = document.createElement('h3');
+        title.textContent = item.name; 
+        coffeeItem.appendChild(title);
+
+        const price = document.createElement('span');
+        price.textContent = `S/.${item.price}`; 
+        coffeeItem.appendChild(price);
+
+        container.appendChild(coffeeItem);
+        coffeeItem.addEventListener("click", ()=>{
+            alert("hola");
+        })
+    });
+}
+
+fetchData().then(data => {
+    generateHTML(data);
+});
