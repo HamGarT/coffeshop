06004b50dbf3bf63d0e366b09bac2d6c5c9a063d
Hamer Garcia <hamgar28@gmail.com>
2024-10-10 02:15:49 -0500

    redesign by me and imer, jesus, julio

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,25 +1,28 @@
 const container = document.getElementById('c-products__coffee-list');
-
+const getRandomSizeClass = () => {
+    const sizes = ['small', 'small-medium', 'medium', 'large'];
+    return sizes[Math.floor(Math.random() * sizes.length)];
+};
 fetch('../src/data/menu.json')
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(coffee => {
-    console.log(coffee);
-    coffee.forEach(coffee => {
-        container.innerHTML += itemMenu(coffee
-        );
-    });
-})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(coffee => {
+        console.log(coffee);
+        coffee.forEach(coffee => {
+            container.innerHTML += itemMenu(coffee
+            );
+        });
+    })
 
-const itemMenu=(content)=>{
-    return(
-        `<div class="c-products__coffee-item-list">
+const itemMenu = (content) => {
+    return (
+        `<div class="c-products__coffee-item-list ${getRandomSizeClass()}">
             <img src="${content.image}" alt="" srcset="">
-            <div>
+            <div class="c-products__info-item">
                 <h3>${content.title}</h3>
                 <p>S/.${content.precio}<span></span></p>
             </div>
