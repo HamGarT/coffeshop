6a307737faa32eb52bdf640d30af490647da86bb
Hamer Garcia <hamgar28@gmail.com>
2024-11-10 01:34:55 -0500

    responsive navbar

--- src/js/components.js
+++ src/js/components.js
@@ -1,44 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
-    const loadHTML = (url, containerId) => {
-      fetch(url)
-        .then(response => response.text())
-        .then(data => {
-          const container = document.getElementById(containerId);
-          if (container) {
-            container.innerHTML = data;
-          } else {
-            console.error(`Element with ID ${containerId} not found.`);
-          }
-        })
-        .catch(error => console.error(`Error loading ${url}:`, error));
-    };
 
-    // Load navbar and footer
-    loadHTML('../src/components/navbar.html', 'navbar-container');
-    loadHTML('../src/components/footer.html', 'footer-container');
 
-    // Handle scroll event for sticky navbar
-    const handleScroll = () => {
-      const navbar = document.getElementById('navbar-container');
-      if (navbar) {
-        if (window.scrollY >= 100) {
-          navbar.classList.add('sticky');
-        } else {
-          navbar.classList.remove('sticky');
-        }
-      }
-    };
+const loadHTML = async (url, containerId, callback) => {
+  try {
+    const container = document.getElementById(containerId);
+    if (!container) {
+      throw new Error(`Container element with ID '${containerId}' not found.`);
+    }
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to load content from ${url}: ${response.statusText}`);
+    }
+    container.innerHTML = await response.text();
+    if (callback && typeof callback === 'function') {
+      callback();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-    // Throttle scroll event handler
-    let isThrottled = false;
-    window.addEventListener('scroll', () => {
-      if (!isThrottled) {
-        handleScroll();
-        isThrottled = true;
-        setTimeout(() => {
-          isThrottled = false;
-        }, 100); // Adjust throttle delay as needed
-      }
-    });
+const handleScroll = () => {
+  let navbar = document.getElementById('navbar-container');
+  if (navbar) {
+    if (window.scrollY >= 100 && window.innerWidth >= 1025) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  }
+};
+
+loadHTML('../src/components/navbar.html', 'navbar-container', () => {
+  const navbarBtn = document.getElementById('navbarbtn');
+  const navbarCLoseBtn = document.getElementById('navbarclosebtn');
+  const navbar = document.querySelector('.c-navbar');
+  navbarBtn.addEventListener('click', () => {
+    navbar.style.width = "100vw"
   });
+  navbarCLoseBtn.addEventListener('click', () => {
+    navbar.style.width = "0"
+  });
+});
+loadHTML('../src/components/footer.html', 'footer-container');
+
+document.addEventListener('DOMContentLoaded', () => {
+  let isThrottled = false;
+  window.addEventListener('scroll', () => {
+    if (!isThrottled) {
+      handleScroll();
+      isThrottled = true;
+      setTimeout(() => {
+        isThrottled = false;
+      }, 100);
+    }
+  });
+});
+
